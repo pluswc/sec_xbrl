b7ace49546f11231aba8ec1d37aa7e419ed7412c
pluswc <koreabdw@gmail.com>
2026-08-24 10:17:35 +0900

    M8: Layer 3 cross-company semantics

---
+++ src/sec_xbrl/cross_company/__init__.py
@@ -0,0 +1,17 @@
+"""Layer 3 cross-company analytical mappings and comparison panels."""
+
+from sec_xbrl.cross_company.mapping import (
+    CROSS_COMPANY_MAPPING_VERSION,
+    ComparisonPanelBuilder,
+    CrossCompanyMapper,
+    CrossCompanyMappingTables,
+    CrossCompanyRelation,
+)
+
+__all__ = [
+    "CROSS_COMPANY_MAPPING_VERSION",
+    "ComparisonPanelBuilder",
+    "CrossCompanyMapper",
+    "CrossCompanyMappingTables",
+    "CrossCompanyRelation",
+]
